30bba90c8cc280f1f454087792011eaba1a08e5e
Dirk Thomas <dthomas@willowgarage.com>
2012-09-25 20:56:40 -0700

    Imported Upstream version 0.4.9

--- setup.py
+++ setup.py
@@ -1,34 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import sys
-from xml.etree.ElementTree import ElementTree
+from catkin_pkg.package import parse_package_for_distutils
 
-try:
-    root = ElementTree(None, 'stack.xml')
-    version = root.findtext('version')
-except Exception, e:
-    print >> sys.stderr, 'Could not extract version from your stack.xml:\n%s' % e
-    sys.exit(-1)
+d = parse_package_for_distutils()
+d['packages'] = ['genmsg']
+d['package_dir'] = {'': 'src'}
 
-sys.path.insert(0, 'src')
-
-setup(name = 'genmsg',
-      version = version,
-      packages = ['genmsg'],
-      package_dir = {'': 'src'},
-      scripts = [],
-      author = "Ken Conley, Josh Faust, Troy Straszheim",
-      author_email = "kwc@willowgarage.com",
-      url = "http://www.ros.org/wiki/genmsg",
-      download_url = "http://pr.willowgarage.com/downloads/genmsg/",
-      keywords = ["ROS"],
-      classifiers = [
-        "Programming Language :: Python",
-        "License :: OSI Approved :: BSD License" ],
-      description = "ROS msg/srv generation",
-      long_description = """\
-Library and scripts for generating ROS message data structures in Python, C++, and Lisp.
-""",
-      license = "BSD"
-      )
+setup(**d)
