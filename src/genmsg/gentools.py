2be96cb4a7c000e125f8d00413609010aaa5d081
Dirk Thomas <dthomas@osrfoundation.org>
2014-01-07 22:14:36 -0800

    Rebase from 'release/hydro/genmsg'

--- src/genmsg/gentools.py
+++ src/genmsg/gentools.py
@@ -100,10 +100,10 @@ def _compute_hash(msg_context, spec, hash):
     # accumulate the hash
     # - root file
     if isinstance(spec, MsgSpec):
-        hash.update(compute_md5_text(msg_context, spec))
+        hash.update(compute_md5_text(msg_context, spec).encode())
     elif isinstance(spec, SrvSpec):
-        hash.update(compute_md5_text(msg_context, spec.request))
-        hash.update(compute_md5_text(msg_context, spec.response))        
+        hash.update(compute_md5_text(msg_context, spec.request).encode())
+        hash.update(compute_md5_text(msg_context, spec.response).encode())
     else:
         raise Exception("[%s] is not a message or service"%spec)   
     return hash.hexdigest()
